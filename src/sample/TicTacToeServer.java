8ce79b1a64cae60ae273ec43c5fdd7c901d29e4c
pete8550 <42993922+pete8550@users.noreply.github.com>
2019-09-05 22:45:17 +0200

    Initial commit

---
+++ src/sample/TicTacToeServer.java
@@ -0,0 +1,172 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.TextArea;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class TicTacToeServer extends Application implements TicTacToeConstants{
+
+    private int sessionNo = 1;
+
+    @Override
+    public void start(Stage stage) {
+        TextArea taLog = new TextArea();
+
+        Scene scene = new Scene(new javafx.scene.control.ScrollPane(taLog), 450, 200);
+        stage.setTitle("TicTacToe Server");
+        stage.setScene(scene);
+        stage.show();
+
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(8000);
+
+                Platform.runLater(() -> taLog.appendText(new Date() +
+                        ": Server started at socket 8000\n"));
+
+                while (true) {
+                    Platform.runLater(() ->taLog.appendText(new Date() +
+                            ": Wait for players to join session " + sessionNo + '\n'));
+
+                    Socket player1 = serverSocket.accept();
+
+                    Platform.runLater(() -> {
+                        taLog.appendText(new Date() + "Player 1 joined session "
+                                + sessionNo + '\n');
+                        taLog.appendText("Player 1's IP address" +
+                                player1.getInetAddress().getHostAddress() + '\n');
+                    });
+
+                    new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER1);
+
+                    Socket player2 = serverSocket.accept();
+
+                    Platform.runLater(() -> {
+                        taLog.appendText(new Date() +
+                                ": Player 2 joined session " + sessionNo + '\n');
+                        taLog.appendText("Player 2's IP address" +
+                                player2.getInetAddress().getHostAddress() + '\n');
+                    });
+
+                    new DataOutputStream(player2.getOutputStream()).writeInt(PLAYER2);
+
+                    Platform.runLater(() ->
+                            taLog.appendText(new Date() +
+                                    ": Start a thread for session " + sessionNo++ + '\n'));
+
+                    new Thread(new HandleASession(player1, player2)).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    class HandleASession implements Runnable, TicTacToeConstants {
+        private Socket player1;
+        private Socket player2;
+
+        private char[][] cell = new char[3][3];
+
+        private DataInputStream fromPlayer1;
+        private DataOutputStream toPlayer1;
+        private DataInputStream fromPlayer2;
+        private DataOutputStream toPlayer2;
+
+        private boolean continueToPlay = true;
+
+        public HandleASession(Socket player1, Socket player2) {
+            this.player1 = player1;
+            this.player2 = player2;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    cell[i][j] = ' ';
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                DataInputStream fromPlayer1 = new DataInputStream(
+                        player1.getInputStream());
+                DataOutputStream toPlayer1 = new DataOutputStream(
+                        player1.getOutputStream());
+                DataInputStream fromPlayer2 = new DataInputStream(
+                        player2.getInputStream());
+                DataOutputStream toPlayer2 = new DataOutputStream(
+                        player2.getOutputStream());
+
+                toPlayer1.writeInt(1);
+
+                while (true) {
+
+                    int row = fromPlayer1.readInt();
+                    int column = fromPlayer1.readInt();
+                    cell[row][column] = 'X';
+
+                    if (isWon('X')) {
+                        toPlayer1.writeInt(PLAYER1_WON);
+                        toPlayer2.writeInt(PLAYER1_WON);
+                        sendMove(toPlayer2, row, column);
+                        break;
+                    }
+                    else if (isFull()) {
+                        toPlayer1.writeInt(DRAW);
+                        toPlayer2.writeInt(DRAW);
+                        sendMove(toPlayer2, row, column);
+                        break;
+                    }
+                    else {
+                        toPlayer2.writeInt(CONTINUE);
+                        sendMove(toPlayer2, row, column);
+                    }
+
+                    row = fromPlayer2.readInt();
+                    column = fromPlayer2.readInt();
+                    cell[row][column] = 'O';
+
+                    if (isWon('O')) {
+                        toPlayer1.writeInt(PLAYER2_WON);
+                        toPlayer2.writeInt(PLAYER2_WON);
+                        sendMove(toPlayer1, row, column);
+                        break;
+                    }
+                    else {
+                        toPlayer1.writeInt(CONTINUE);
+                        sendMove(toPlayer1, row, column);
+                    }
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void sendMove(DataOutputStream out, int row, int column) throws IOException {
+            out.writeInt(row);
+            out.writeInt(column);
+        }
+
+        private boolean isFull() {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (cell[i][j] == ' ')
+                        return false;
+
+                    return true;
+        }
+
+
+    }
+
+}
